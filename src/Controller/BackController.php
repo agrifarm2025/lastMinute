0764012befd6860b865eadd18a72bf347e1c5156
Majdayedi <146891176+Majdayedi@users.noreply.github.com>
2025-02-19 08:06:52 +0100

    last

--- src/Controller/BackController.php
+++ src/Controller/BackController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\FarmRepository;
+use App\Repository\FieldRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -29,6 +30,31 @@ final class BackController extends AbstractController
         return $this->render('back/field.html.twig', [  
             'farm'=>$farm ,'fields'=>$fields
         ]);  
+    }
+    #[Route('/joinf/{id}', name: 'joinf')]  
+    public function show_fields(FieldRepository $fieldRepository, $id): Response
+    {
+        $field = $fieldRepository->find($id);
+        if (!$field) {
+            throw $this->createNotFoundException('No field found for id ' . $id);
+        }
+    
+       
+        foreach ($field as $fields) {
+            $profit = $fields->getIncome() - $fields->getOutcome(); // Calculate profit
+            $fields->setProfit($profit);  // Assuming you have a setter for profit in your Field entity
+        }
+        $todo = $fieldRepository->getToDo($field);
+        $inprogress = $fieldRepository->getInProgress($field);
+        $done = $fieldRepository->getDone($field);
+    
+        return $this->render('back/task.html.twig', [
+            'id' => $field->getFarm()->getId(),
+            'field' => $field,
+            'todo' => $todo,
+            'inprogress' => $inprogress,
+            'done' => $done
+        ]);
     }
     
     
